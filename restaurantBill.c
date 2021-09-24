46a51ee50b443181963b61c132eada4e8626895d
Huijun Hu <huijunhu@cpp.edu>
2021-09-23 21:52:51 -0700

    Initial commit - outline

---
+++ restaurantBill.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+Restaurant Bill
+@ author : Huijun Hu
+ - computes the tax and tip on a restaurant bill for a patron. 
+ - accept the tax and tip both in percentages as input. 
+ - Display the meal cost, tax amount, tip amount, and total bill. 
+ - The meal cost should be randomly selected.
+ */
+ 
